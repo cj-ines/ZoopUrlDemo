eaddfac6c0d431c6a5a58839eb6e282483fe406e
Crisdel James Ines <cj.ines@outlook.ph>
2014-06-19 14:55:43 +0800

    add czoopurl

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  'doctrine' => array(
+    'connection' => array(
+      'orm_default' => array(
+        'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
+        'params' => array(
+          'host'     => 'mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com',
+          'port'     => '3306',
+          'user'     => 'depf7gq3vad',
+          'password' => 'oVIhCADF7jIl',
+          'dbname'   => 'depf7gq3vad',
+)))));
+
